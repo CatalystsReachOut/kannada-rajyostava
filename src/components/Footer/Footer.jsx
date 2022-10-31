3477bbb1008a869160baa8d40d9549c2d98d37e3
AnandD17 <avdoddamani123@gmail.com>
2022-10-31 12:01:57 +0530

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='h-[50px] w-full bg-transparent p-6 mb-6'>
-        <div>
+        <div className='text-right'>
             Designed and developed by &nbsp;&nbsp;&nbsp; <a href="https://catalystsreachout.com/" className='text-[red]'> Catalysts Reachout </a>
         </div>
     </div>
